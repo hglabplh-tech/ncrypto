f984c846c9736d70344379932b5a6d5016e66e92
Harald Glab-Plhak <harald.glab-plhak@t-online.de>
2023-07-31 23:34:34 +0200

    finished encode setup coding

--- ncrypto-lib/private/cipher/aes-cipher.rkt
+++ ncrypto-lib/private/cipher/aes-cipher.rkt
@@ -33,33 +33,122 @@
     ret-vect))
 
 (define (round-calc128 round-key-vect rk-add)
-    (let round-128 ([rkey-vect round-key-vect]
-                    [index 0]
-                    [rk-offset 0])
-      (cond [(< index 10)
-      (let* ([temp (vector-ref rkey-vect (rk-offset + 3))]
-             [rkey-vect-int (vector-append rkey-vect
-                                           (make-vector 1
-                                                        ( bitwise-xor
-                                                         (vector-ref rkey-vect (rk-offset + 0))
-                                                         (bitwise-and (list-ref list-te4-encr
-                                                                   (bitwise-and (>> temp 16) #xff))#xff000000)
-                                                         (bitwise-and (list-ref list-te4-encr
-                                                                   (bitwise-and (>> temp 8) #xff)) #x00ff0000)
-                                                         (bitwise-and (list-ref list-te4-encr
-                                                                   (bitwise-and (>> temp 0) #xff)) #x0000ff00)
-                                                         (bitwise-and (list-ref list-te4-encr
-                                                                   (bitwise-and (>> temp 24) #xff))#x000000ff)
-                                                            (list-ref rcon index))))]
-             [rkey-vect-int (vector-append (make-vector 1 (bitwise-xor (vector-ref rkey-vect-int (rk-offset + 1))
-                                                        (vector-ref rkey-vect-int (rk-offset + 4))))
-                                           (make-vector 1 (bitwise-xor (vector-ref rkey-vect-int (rk-offset + 2))
-                                                        (vector-ref rkey-vect-int (rk-offset + 5))))
-                                           (make-vector 1 (bitwise-xor (vector-ref rkey-vect-int (rk-offset + 3))
-                                                        (vector-ref rkey-vect-int (rk-offset + 6))))
-                                           )])
-        (round-128 rkey-vect-int (add1 index) (+ rk-offset 4)))]
-            [rkey-vect])))
+  (let round-128 ([rkey-vect round-key-vect]
+                  [index 0]
+                  [rk-offset 0])
+    (cond [(< index 10)
+           (let* ([temp (vector-ref rkey-vect (+ rk-offset 3))]
+                  [rkey-vect-int (vector-append rkey-vect
+                                                (make-vector 1
+                                                             ( bitwise-xor
+                                                               (vector-ref rkey-vect (rk-offset + 0))
+                                                               (bitwise-and (list-ref list-te4-encr
+                                                                                      (bitwise-and (>> temp 16) #xff))#xff000000)
+                                                               (bitwise-and (list-ref list-te4-encr
+                                                                                      (bitwise-and (>> temp 8) #xff)) #x00ff0000)
+                                                               (bitwise-and (list-ref list-te4-encr
+                                                                                      (bitwise-and (>> temp 0) #xff)) #x0000ff00)
+                                                               (bitwise-and (list-ref list-te4-encr
+                                                                                      (bitwise-and (>> temp 24) #xff))#x000000ff)
+                                                               (list-ref rcon index))))]
+                  [rkey-vect-int (vector-append (make-vector 1 (bitwise-xor (vector-ref rkey-vect-int (rk-offset + 1))
+                                                                            (vector-ref rkey-vect-int (rk-offset + 4))))
+                                                (make-vector 1 (bitwise-xor (vector-ref rkey-vect-int (rk-offset + 2))
+                                                                            (vector-ref rkey-vect-int (rk-offset + 5))))
+                                                (make-vector 1 (bitwise-xor (vector-ref rkey-vect-int (rk-offset + 3))
+                                                                            (vector-ref rkey-vect-int (rk-offset + 6))))
+                                                )])
+             (round-128 rkey-vect-int (add1 index) (+ rk-offset rk-add)))]
+          [rkey-vect])))
+
+(define (round-calc192 round-key-vect rk-add)
+  (let round-192 ([rkey-vect round-key-vect]
+                  [index 0]
+                  [rk-offset 0])
+     
+    (let* ([temp (vector-ref rkey-vect (+ rk-offset 5))]
+           [rkey-vect-int (vector-append rkey-vect
+                                         (make-vector 1
+                                                      ( bitwise-xor
+                                                        (vector-ref rkey-vect (rk-offset + 0))
+                                                        (bitwise-and (list-ref list-te4-encr
+                                                                               (bitwise-and (>> temp 16) #xff))#xff000000)
+                                                        (bitwise-and (list-ref list-te4-encr
+                                                                               (bitwise-and (>> temp 8) #xff)) #x00ff0000)
+                                                        (bitwise-and (list-ref list-te4-encr
+                                                                               (bitwise-and (>> temp 0) #xff)) #x0000ff00)
+                                                        (bitwise-and (list-ref list-te4-encr
+                                                                               (bitwise-and (>> temp 24) #xff))#x000000ff)
+                                                        (list-ref rcon index))))]
+           [rkey-vect-int (vector-append (make-vector 1 (bitwise-xor (vector-ref rkey-vect-int (rk-offset + 1))
+                                                                     (vector-ref rkey-vect-int (rk-offset + 6))))
+                                         (make-vector 1 (bitwise-xor (vector-ref rkey-vect-int (rk-offset + 2))
+                                                                     (vector-ref rkey-vect-int (rk-offset + 7))))
+                                         (make-vector 1 (bitwise-xor (vector-ref rkey-vect-int (rk-offset + 3))
+                                                                     (vector-ref rkey-vect-int (rk-offset + 8))))
+                                         )])
+      (cond [(eq? index 7)
+             rkey-vect]
+            [(let* ([rkey-vect-int (vector-append (make-vector 1 (bitwise-xor (vector-ref rkey-vect-int (rk-offset + 4))
+                                                                              (vector-ref rkey-vect-int (rk-offset + 9))))
+                                                  (make-vector 1 (bitwise-xor (vector-ref rkey-vect-int (rk-offset + 10))
+                                                                              (vector-ref rkey-vect-int (rk-offset + 7)))))])
+               (round-192 rkey-vect-int (add1 index) (+ rk-offset rk-add)))]
+               
+            ))))
+
+(define (round-calc256 round-key-vect rk-add)
+  (let round-192 ([rkey-vect round-key-vect]
+                  [index 0]
+                  [rk-offset 0])
+     
+    (let* ([temp (vector-ref rkey-vect (+ rk-offset 7))]
+           [rkey-vect-int (vector-append rkey-vect
+                                         (make-vector 1
+                                                      ( bitwise-xor
+                                                        (vector-ref rkey-vect (rk-offset + 0))
+                                                        (bitwise-and (list-ref list-te4-encr
+                                                                               (bitwise-and (>> temp 16) #xff))#xff000000)
+                                                        (bitwise-and (list-ref list-te4-encr
+                                                                               (bitwise-and (>> temp 8) #xff)) #x00ff0000)
+                                                        (bitwise-and (list-ref list-te4-encr
+                                                                               (bitwise-and (>> temp 0) #xff)) #x0000ff00)
+                                                        (bitwise-and (list-ref list-te4-encr
+                                                                               (bitwise-and (>> temp 24) #xff))#x000000ff)
+                                                        (list-ref rcon index))))]
+           [rkey-vect-int (vector-append (make-vector 1 (bitwise-xor (vector-ref rkey-vect-int (rk-offset + 1))
+                                                                     (vector-ref rkey-vect-int (rk-offset + 8))))
+                                         (make-vector 1 (bitwise-xor (vector-ref rkey-vect-int (rk-offset + 2))
+                                                                     (vector-ref rkey-vect-int (rk-offset + 9))))
+                                         (make-vector 1 (bitwise-xor (vector-ref rkey-vect-int (rk-offset + 3))
+                                                                     (vector-ref rkey-vect-int (rk-offset + 10))))
+                                         )])
+      (cond [(eq? index 6)
+             rkey-vect]
+            [(let* ([rkey-vect-int (vector-append
+                                    (make-vector 1
+                                                 ( bitwise-xor
+                                                   (vector-ref rkey-vect (rk-offset + 0))
+                                                   (bitwise-and (list-ref list-te4-encr
+                                                                          (bitwise-and (>> temp 24) #xff))#xff000000)
+                                                   (bitwise-and (list-ref list-te4-encr
+                                                                          (bitwise-and (>> temp 16) #xff))#x00ff0000)
+                                                   (bitwise-and (list-ref list-te4-encr
+                                                                          (bitwise-and (>> temp 8) #xff)) #x0000ff00)
+                                                   (bitwise-and (list-ref list-te4-encr
+                                                                          (bitwise-and (>> temp 0) #xff)) #x000000ff)
+                                                   (list-ref rcon index)))
+                                    (make-vector 1 (bitwise-xor (vector-ref rkey-vect-int (rk-offset + 4))
+                                                                (vector-ref rkey-vect-int (rk-offset + 12))))
+                                    (make-vector 1 (bitwise-xor (vector-ref rkey-vect-int (rk-offset + 10))
+                                                                (vector-ref rkey-vect-int (rk-offset + 13))))
+                                    (make-vector 1 (bitwise-xor (vector-ref rkey-vect-int (rk-offset + 10))
+                                                                (vector-ref rkey-vect-int (rk-offset + 14)))))])
+               (round-192 rkey-vect-int (add1 index) (+ rk-offset rk-add)))]
+               
+            ))))
+
+            
             
                     
 
@@ -73,5 +162,25 @@
          [round-key-vect (list->vector round-key)])
     (cond [(eq? key-bits 128)
            (values 10 (round-calc128 round-key-vect 4))])
-    round-key)) ;; here follows the calculation !!!
-    
+    (let* ([round-key-vect (vector-append
+                            round-key-vect
+                            (make-vector 1
+                                         (bytes->32-big (subbytes cipher-key 16 4))))]
+           [round-key-vect (vector-append
+                            round-key-vect
+                            (make-vector 1
+                                         (bytes->32-big (subbytes cipher-key 20 4))))]
+           (cond [(eq? key-bits 192)
+                  (values 12 (round-calc192 round-key-vect 6))]))
+      (let* ([round-key-vect (vector-append
+                              round-key-vect
+                              (make-vector 1
+                                           (bytes->32-big (subbytes cipher-key 24 4))))]
+             [round-key-vect (vector-append
+                              round-key-vect
+                              (make-vector 1
+                                           (bytes->32-big (subbytes cipher-key 28 4))))])
+        (cond [(eq? key-bits 256)
+               (values 14 (round-calc256 round-key-vect 8))]))
+           
+      round-key))) ;; here follows the calculation !!!
